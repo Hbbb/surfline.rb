7af1be4589ee91ad9fadd5ee0e87caf672751d1d
Hbbb <h.borges10592@gmail.com>
2016-02-27 22:46:48 -0800

    Add support for regions

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -6,18 +6,9 @@ describe Surfline::Client do
     expect(Surfline::Client::BASE_URI).to eq('http://api.surfline.com/v1/forecasts')
   end
 
-  context 'lookup using string' do
-    it 'fetches' do
-      data = JSON.parse(Surfline::Client.send(:fetch, 4248, 'surf'))
-
-      expect(data).not_to be_empty
-      ['Surf', 'Location', 'lat', 'lon'].each do |key|
-        expect(data).to have_key(key)
-      end
-    end
-
+  shared_examples 'surflineclient' do |lookup_key|
     it 'fetches surf' do
-      swell_data = JSON.parse(Surfline::Client.surf('windandsea'))
+      swell_data = JSON.parse(Surfline::Client.surf(lookup_key))
       expect(swell_data).not_to be_empty
       ['Surf', 'Location', 'lat', 'lon'].each do |key|
         expect(swell_data).to have_key(key)
@@ -25,7 +16,7 @@ describe Surfline::Client do
     end
 
     it 'fetches tide' do
-      tide_data = JSON.parse(Surfline::Client.tide('windandsea'))
+      tide_data = JSON.parse(Surfline::Client.tide(lookup_key))
       expect(tide_data).not_to be_empty
       ['Tide', 'Location', 'lat', 'lon'].each do |key|
         expect(tide_data).to have_key(key)
@@ -33,7 +24,7 @@ describe Surfline::Client do
     end
 
     it 'fetches wind' do
-      wind_data = JSON.parse(Surfline::Client.wind('windandsea'))
+      wind_data = JSON.parse(Surfline::Client.wind(lookup_key))
       expect(wind_data).not_to be_empty
       ['Wind', 'Location', 'lat', 'lon'].each do |key|
         expect(wind_data).to have_key(key)
@@ -41,56 +32,25 @@ describe Surfline::Client do
     end
 
     it 'fetches analysis' do
-      analysis_data = JSON.parse(Surfline::Client.analysis('windandsea'))
+      analysis_data = JSON.parse(Surfline::Client.analysis(lookup_key))
       expect(analysis_data).not_to be_empty
       ['Analysis', 'Location', 'lat', 'lon'].each do |key|
         expect(analysis_data).to have_key(key)
       end
     end
+
   end
 
   context 'lookup using id' do
-    it 'fetches' do
-      data = JSON.parse(Surfline::Client.send(:fetch, 4248, 'surf'))
-
-      expect(data).not_to be_empty
-      ['Surf', 'Location', 'lat', 'lon'].each do |key|
-        expect(data).to have_key(key)
-      end
-    end
-
-    it 'fetches surf' do
-      swell_data = JSON.parse(Surfline::Client.surf(4248))
-      expect(swell_data).not_to be_empty
-      ['Surf', 'Location', 'lat', 'lon'].each do |key|
-        expect(swell_data).to have_key(key)
-      end
-    end
-
-    it 'fetches tide' do
-      tide_data = JSON.parse(Surfline::Client.tide(4248))
-      expect(tide_data).not_to be_empty
-      ['Tide', 'Location', 'lat', 'lon'].each do |key|
-        expect(tide_data).to have_key(key)
-      end
-    end
-
-    it 'fetches wind' do
-      wind_data = JSON.parse(Surfline::Client.wind(4248))
-      expect(wind_data).not_to be_empty
-      ['Wind', 'Location', 'lat', 'lon'].each do |key|
-        expect(wind_data).to have_key(key)
-      end
-    end
-
-    it 'fetches analysis' do
-      analysis_data = JSON.parse(Surfline::Client.analysis(4248))
-      expect(analysis_data).not_to be_empty
-      ['Analysis', 'Location', 'lat', 'lon'].each do |key|
-        expect(analysis_data).to have_key(key)
-      end
-    end
+    include_examples 'surflineclient', 4248
   end
 
+  context 'lookup using regions' do
+    include_examples 'surflineclient', 2953
+  end
+
+  context 'lookup using string' do
+    include_examples 'surflineclient', 'windandsea'
+  end
 
 end
